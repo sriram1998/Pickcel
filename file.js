8c0478c5bc8f536f35434d13981a8be53e939f0e
sriram1998 <xmenram@gmail.com>
2018-05-10 12:46:21 +0530

    Update file.js

--- file.js
+++ file.js
@@ -8,7 +8,7 @@ var mysql=require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "narutorama",
+  password: "Enter password here",
   database: "file"
 });
 
