b75992defc8ac65f89df717659aa0a27a25133e2
sriram1998 <xmenram@gmail.com>
2018-05-10 12:45:54 +0530

    Update table.js

--- table.js
+++ table.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "narutorama",
+  password: "Enter password here",
   database: "file"
 });
 
